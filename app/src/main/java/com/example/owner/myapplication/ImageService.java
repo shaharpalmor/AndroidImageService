f5437c9dd93cf6c97c38d54257082bc05fc6a2fa
shahar palmor <shaharpalmor@gmail.com>
2018-06-12 17:03:55 +0300

    ch ch ch changes

--- app/src/main/java/com/example/owner/myapplication/ImageService.java
+++ app/src/main/java/com/example/owner/myapplication/ImageService.java
@@ -1,16 +1,21 @@
 package com.example.owner.myapplication;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.Toast;
 
 public class ImageService extends Service {
 
-
-    public ImageService() {
-    }
+    private BroadcastReceiver broadcastReceiver;
+    public ImageService() { }
 
     @Nullable
     @Override
@@ -20,9 +25,27 @@ public class ImageService extends Service {
 
     public void onCreate() {
         super.onCreate();
-        // Here put the Code of Service
-    }
 
+        final IntentFilter theFilter = new IntentFilter();
+        theFilter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        theFilter.addAction("android.net.wifi.STATE_CHANGE");
+        this.broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager)context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            TcpConnection tcpConnection = new TcpConnection();
+                            tcpConnection.startConnection();
+                        }
+                    }
+                }
+            }
+        };
+        this.registerReceiver(this.broadcastReceiver, theFilter);
+    }
 
     public int onStartCommand(Intent intent, int flag, int startId) {
         Toast.makeText(this, "Service starting...", Toast.LENGTH_SHORT).show();
---
+++ app/src/main/java/com/example/owner/myapplication/TcpConnection.java
@@ -0,0 +1,59 @@
+package com.example.owner.myapplication;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TcpConnection {
+    public void startConnection() {
+        try {
+            InetAddress serverAddr = InetAddress.getByName("10.0.0.2");
+
+            Socket socket = new Socket(serverAddr, 6666);
+            try {
+                OutputStream output = socket.getOutputStream();
+
+                File DCIM = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+                if (DCIM == null) {
+                    return;
+                }
+                File[] pics = DCIM.listFiles();
+                int count = 0;
+                if (pics != null) {
+                    for (File pic : pics) {
+
+                        FileInputStream fis = new FileInputStream(pic);
+                        Bitmap bitmap = BitmapFactory.decodeStream(fis);
+                        byte[] imgByte = getBytesFromBitmap(bitmap);
+
+                        output.write(imgByte);
+                        output.flush();
+                        ++count;
+                    }
+                }
+            } catch (Exception e) {
+                Log.e("TCP", "C: Error", e);
+            } finally {
+                socket.close();
+            }
+
+        } catch (Exception e) {
+            Log.e("TCP", "C: Error", e);
+        }
+    }
+
+    private byte[] getBytesFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 70, stream);
+        return stream.toByteArray();
+
+    }
+}
